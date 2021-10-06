cca8de1d4c9cbd9c2f145de4b2a653d2465f64ef
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-10-06 21:48:58 +0300

    creating ui WelcomeActivity

--- app/src/main/java/com/tsaryk/mytaxi/WelcomeActivity.java
+++ app/src/main/java/com/tsaryk/mytaxi/WelcomeActivity.java
@@ -3,12 +3,17 @@ package com.tsaryk.mytaxi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button driverBtn, customerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
     }
 }
