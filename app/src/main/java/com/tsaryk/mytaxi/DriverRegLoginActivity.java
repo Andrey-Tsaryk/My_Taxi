3ce3cc1eedba2116586f6003c5fff7da98eb3e58
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-10-10 19:25:35 +0300

    added logit fot DriverRegLoginActivity

--- app/src/main/java/com/tsaryk/mytaxi/DriverRegLoginActivity.java
+++ app/src/main/java/com/tsaryk/mytaxi/DriverRegLoginActivity.java
@@ -3,12 +3,41 @@ package com.tsaryk.mytaxi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DriverRegLoginActivity extends AppCompatActivity {
+
+    TextView driverStatus, question;
+    Button signInBtn, signUpBtn;
+    EditText emailET, passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_reg_login);
+
+        driverStatus = (TextView)findViewById(R.id.statusDriver);
+        question = (TextView)findViewById(R.id.accountCreate);
+        signInBtn = (Button) findViewById(R.id.signInDriver);
+        signUpBtn = (Button) findViewById(R.id.signUpDriver);
+        emailET = (EditText) findViewById(R.id.driverEmail);
+        passwordET = (EditText) findViewById(R.id.driverPassword);
+
+        signUpBtn.setVisibility(View.INVISIBLE);  //кнопки не видна
+        signUpBtn.setEnabled(false);              // она не нажимается!!!
+
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInBtn.setVisibility(View.INVISIBLE);
+                question.setVisibility(View.INVISIBLE);
+                signInBtn.setVisibility(View.VISIBLE);
+                signUpBtn.setEnabled(true);
+                driverStatus.setText("Registration for driver");
+            }
+        });
     }
 }
