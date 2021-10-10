78388beb2f5a9818fde8566f11a4beafff3442b6
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-10-10 20:04:47 +0300

    added logic and UI for customer(xml, activity.java)

--- app/src/main/java/com/tsaryk/mytaxi/CustomerRegLoginActivity.java
+++ app/src/main/java/com/tsaryk/mytaxi/CustomerRegLoginActivity.java
@@ -3,12 +3,41 @@ package com.tsaryk.mytaxi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CustomerRegLoginActivity extends AppCompatActivity {
+
+    TextView customerStatus, question;
+    Button signInBtn, signUpBtn;
+    EditText emailET, passwordET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_reg_login);
+
+        customerStatus = (TextView)findViewById(R.id.statusCustomer);
+        question = (TextView)findViewById(R.id.accountCreateCustomer);
+        signInBtn = (Button) findViewById(R.id.signInCustomer);
+        signUpBtn = (Button) findViewById(R.id.signUpCustomer);
+        emailET = (EditText) findViewById(R.id.customerEmail);
+        passwordET = (EditText) findViewById(R.id.customerPassword);
+
+        signUpBtn.setVisibility(View.INVISIBLE);
+        signUpBtn.setEnabled(false);
+
+        question.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInBtn.setVisibility(View.INVISIBLE);
+                question.setVisibility(View.INVISIBLE);
+                signInBtn.setVisibility(View.VISIBLE);
+                signUpBtn.setEnabled(true);
+                customerStatus.setText("Registration for customer");
+            }
+        });
     }
 }
